ccb6abb1288b50414ff8427fb0ffa164dc44eb81
phatak-kaustubh <k08phatak@gmail.com>
2025-03-18 17:21:41 +0530

    logger correction

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,10 @@
 import sys
-import logging
+
+#import  custom logger
+from logger import logging
+
+
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
