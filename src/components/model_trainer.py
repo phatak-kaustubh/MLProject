3cbddb1a13d1227e41cb33026584a30eae1a062f
phatak-kaustubh <k08phatak@gmail.com>
2025-03-30 14:09:12 +0530

    Hyper parameter tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -52,21 +52,64 @@ class ModelTrainer:
                 "Decision Tree":DecisionTreeRegressor(),
                 "GradientBoostingRegressor":GradientBoostingRegressor(),
                 "LinearRegression":LinearRegression(),
-                "KNeighborsRegressor":KNeighborsRegressor(),
                 "XGBRegressor":XGBRegressor(),
                 "AdaBoostRegressor":AdaBoostRegressor(),
                 #Not done hyper parameter tuning 
                 # to try it by yourself
             }
+            #Hyper parameter tuning 
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                # "CatBoosting Regressor":{
+                #     'depth': [6,8,10],
+                #     'learning_rate': [0.01, 0.05, 0.1],
+                #     'iterations': [30, 50, 100]
+                # },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            
+            
+            
+            
             
             #-evaluate_model function in utils 
             #get the r2 score for all the models in the form of dictionary
+            #for hyper paramter we add 1 more parameter
             model_report:dict=evaluate_models(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models)
+                models=models,
+                param=params)
             
             #Will get the best model score from the dictionry
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,10 @@ import numpy as np
 import pandas as pd
 import dill
 
+#for hyper parameter tuing
+from sklearn.model_selection import GridSearchCV
+
+
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 
@@ -18,16 +22,24 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e.sys)
-    
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+
+
+#Update the code for Hyperparameter tuning    
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
         
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            #
+            para=param[list(models.keys())[i]]
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
             
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-            
+
+            #model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
